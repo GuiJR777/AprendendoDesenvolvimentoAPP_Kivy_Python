1dd134fe56b8bfe474a599ac3062488f72d03c9d
Guilherme J. Ramires <guilhermejramires@gmail.com>
2020-06-01 12:40:59 -0300

    Adicionado_Estrutura_em_Arquivos_Diferentes

--- main.py
+++ main.py
@@ -6,30 +6,14 @@ os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
 #Imports
 from kivy.app import App
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 
-#Classe que constroi a tela
-class Test(App): #A declaração antes de app é o nome da window
+#Montador Kivy
+class Incrementador (BoxLayout):
+    pass
+
+class Test(App):
     def build(self):
-        #Montagem de Botões e Widgets
-        box = BoxLayout(orientation= 'vertical')
-        button = Button(text= 'Botão 1', font_size= 30, 
-        on_release= self.incrementar)
-        self.label= Label(text= '1', font_size= 30)
-        box.add_widget(self.label)
-        box.add_widget (button)
+        return Incrementador()
 
-        
-        #Montagem da Tela Completa
-        
-        return box
-
-    def incrementar(self,button):
-        button.text= 'soltei'
-        self.label.text= str(int(self.label.text)+1)
-
-
-#Invocar Tela
 Test().run()
